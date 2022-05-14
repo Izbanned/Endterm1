8d2e9a693d09645a270c957143af8e04f97bc56a
Dias <201750@astanait.edu.kz>
2022-05-14 15:23:42 +0600

    Initial Commit

---
+++ Endterm1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Endterm1
+//
+//  Created by Dias Karimov on 14.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
